40e1102d5eee9d257ae36d0e106ae3496901109c
unknown <ytw1214@naver.com>
2026-02-04 14:26:57 +0900

    크롤링 Infra 리팩토링

---
+++ src/main/java/com/exerciting/Exerciting/Exception/CrawlingException.java
@@ -0,0 +1,7 @@
+package com.exerciting.Exerciting.Exception;
+
+public class CrawlingException extends RuntimeException {
+    public CrawlingException(String message, Throwable cause) {
+        super(message,cause);
+    }
+}
---
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/Crawler/CrawlerHelper.java
@@ -0,0 +1,52 @@
+package com.exerciting.Exerciting.Infrastructure.Crawler;
+
+import com.exerciting.Exerciting.Exception.CrawlingException;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.springframework.stereotype.Component;
+
+import javax.net.ssl.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+@Component
+public class CrawlerHelper {
+
+
+    public Connection createSafeConnection(String url) {
+        try {
+            applySetting();
+            return Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                    .header("Accept-Language", "ko-KR,ko;q=0.9")
+                    .timeout(10000);
+        } catch (Exception e) {
+            throw new CrawlingException("연걸 실패" + url, e);
+        }
+    }
+    private void applySetting() throws NoSuchAlgorithmException, KeyManagementException {
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return null; }
+
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+                }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new SecureRandom());
+
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) { return true; }
+        });
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+}
