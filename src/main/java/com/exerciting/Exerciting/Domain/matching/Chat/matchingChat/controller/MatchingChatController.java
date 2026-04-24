061dbc2ced2d3294c29b895e547288c409d93a06
unknown <ytw1214@naver.com>
2026-04-24 16:54:51 +0900

    wip : handlemessage 구현중

--- src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/controller/MatchingChatController.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/controller/MatchingChatController.java
@@ -1,13 +1,19 @@
 package com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.controller;
 
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.dto.ChatMessageDto;
+import com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.dto.ChatMessageResponseDto;
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.repository.MatchingChatRepository;
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.service.MatchingChatService;
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatoom.entity.MatchingChatRoom;
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatoom.repository.MatchingChatRoomRepository;
+import com.exerciting.Exerciting.Domain.user.entity.User;
+import com.exerciting.Exerciting.Exception.MatchingChatRoomNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,6 +26,10 @@ public class MatchingChatController {
             ChatMessageDto dto) {
         MatchingChatRoom chatroom = matchingChatRoomRepository.findById(chatRoomId)
                 .orElseThrow (() -> new MatchingChatRoomNotFoundException("해당 채팅방을 찾을 수 없습니다."));
-        matchingChatService.saveMessage(chatroom, sender, dto.message());
+        matchingChatService.saveMessage(chatroom, dto.senderId(), dto.message());
+    }
+    @MessageMapping("/chat/{chatRoomId}")
+    public ChatMessageResponseDto handleMessage(Principal principal) {
+        User sender = userRepository.findById(Long.parseLong(principal.getName()))
     }
 }
