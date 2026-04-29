af2d0b179d0122942950ddb7030450060efbb2ff
unknown <ytw1214@naver.com>
2026-04-29 16:47:42 +0900

    refactor : 응답, 요청 DTO 필드 변경

--- src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/dto/ChatMessageRequestDto.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/dto/ChatMessageRequestDto.java
@@ -2,10 +2,8 @@ package com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.dto;
 
 import java.time.LocalDateTime;
 
-public record ChatMessageDto(
-        Long senderId,
-        String message,
-        LocalDateTime sendAt
+public record ChatMessageRequestDto(
+        String message
 ) {
 
 }
--- src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/dto/ChatMessageResponseDto.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/dto/ChatMessageResponseDto.java
@@ -3,8 +3,10 @@ package com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.dto;
 import java.time.LocalDateTime;
 
 public record ChatMessageResponseDto(
+        Long senderId,
         String message,
         LocalDateTime sendAt,
-        String senderName
+        String senderName,
+        boolean isRead
 ) {
 }
