b77e35ef53c184d64f0123267fc0fafe182125d3
unknown <ytw1214@naver.com>
2026-04-30 17:59:57 +0900

    feat : 메서드 구현

--- src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/repository/MatchingChatRepository.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/repository/MatchingChatRepository.java
@@ -2,11 +2,16 @@ package com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.repository;
 
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.entity.MatchingChat;
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatoom.entity.MatchingChatRoom;
+import com.exerciting.Exerciting.Domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MatchingChatRepository extends JpaRepository<MatchingChat, Long> {
+    @Query("select c from matchingchat c join fetch c.sender where c.matchingChatRoom=:chatroom order by c.sendAt asc")
     List<MatchingChat> findByMatchingChatRoomOrderBySendAtAsc(MatchingChatRoom matchingChatRoom);
+
+    List<MatchingChat> findByMatchingChatRoomAndIsReadFalseAndSenderNot(MatchingChatRoom chatRoom, User sender);
 }
