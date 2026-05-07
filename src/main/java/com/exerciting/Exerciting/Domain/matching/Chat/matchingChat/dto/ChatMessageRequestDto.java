6fe7f2ca683daa3b7898eed75b892545916b3b5b
ytw1214 <ytw1214@naver.com>
2026-05-07 17:14:47 +0900

    refactor : dto 필드 삭제

--- src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/dto/ChatMessageRequestDto.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/dto/ChatMessageRequestDto.java
@@ -3,7 +3,6 @@ package com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.dto;
 import java.time.LocalDateTime;
 
 public record ChatMessageRequestDto(
-        Long senderId,
         String message
 ) {
 
