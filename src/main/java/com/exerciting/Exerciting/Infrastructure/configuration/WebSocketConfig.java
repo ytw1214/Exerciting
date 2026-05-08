35a5294cc67dea4e8fec1f44a8b6f656cc8b55f0
unknown <ytw1214@naver.com>
2026-05-08 17:45:03 +0900

    feat : stomp에 알맞은 configureClientInboundChannel 로직 구현

--- src/main/java/com/exerciting/Exerciting/Infrastructure/configuration/WebSocketConfig.java
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/configuration/WebSocketConfig.java
@@ -1,13 +1,26 @@
 package com.exerciting.Exerciting.Infrastructure.configuration;
 
+import com.exerciting.Exerciting.Domain.matching.Chat.matchingChat.entity.MatchingChat;
+import com.exerciting.Exerciting.Infrastructure.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.*;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JwtTokenProvider jwtTokenProvider;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
@@ -19,5 +32,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
         config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if(accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String token = accessor.getFirstNativeHeader("Authorization");
+                    if(token == null || jwtTokenProvider.validateToken(token.replace("Bearer ", ""))) {
+                        throw new MessagingException("인증에 실패했습니다.");
+                    }
+                }
+                return message;
+            }
+        });
+
     }
 }
