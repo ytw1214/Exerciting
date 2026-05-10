1b5c9ce9ff5cdd371af0c1c319fe82c4c84ba6fa
unknown <ytw1214@naver.com>
2026-05-10 17:19:00 +0900

    refactor : ws에 따른 허용 범위 변경

--- src/main/java/com/exerciting/Exerciting/Infrastructure/security/SecurityConfig.java
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
                         //.requestMatchers("/user/login","/user/signup","/api/v1/games").permitAll()
                         .anyRequest().authenticated()
                 )
