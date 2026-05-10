0e9dd6470288589dcf375ee367220735f5ca175c
unknown <ytw1214@naver.com>
2026-05-10 17:16:58 +0900

    refactor : UserDetails를 활용한 saveMatching 리팩토링

--- src/main/java/com/exerciting/Exerciting/Domain/matching/matching/controller/MatchingController.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/matching/controller/MatchingController.java
@@ -4,7 +4,12 @@ import com.exerciting.Exerciting.Domain.matching.matching.dto.MatchingRequestDto
 import com.exerciting.Exerciting.Domain.matching.matching.entity.Matching;
 import com.exerciting.Exerciting.Domain.matching.matching.service.MatchingService;
 import com.exerciting.Exerciting.Domain.matching.matching.dto.MatchingResponseDto;
+import com.exerciting.Exerciting.Domain.user.entity.User;
+import com.exerciting.Exerciting.Domain.user.entity.UserDetails;
+import com.exerciting.Exerciting.Domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,19 +20,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class MatchingController {
     private final MatchingService matchingService;
-
-    public MatchingController(MatchingService matchingService) {
-        this.matchingService = matchingService;
-    }
-
-
-
+    private final UserService userService;
 
     @PostMapping("/api/v1/Matching")
-    public ResponseEntity<Long> saveMatching(@RequestBody MatchingRequestDto dto, Long hostId) {
-        Long data = matchingService.createMatching(dto, hostId);
+    public ResponseEntity<Long> saveMatching(
+            @RequestBody MatchingRequestDto dto,
+            @AuthenticationPrincipal UserDetails userDetails) {
+        User user = userService.getUserByUserId(userDetails.getUserId());
+        Long data = matchingService.createMatching(dto, user.getId());
+
         return ResponseEntity.ok(data);
     }
 
