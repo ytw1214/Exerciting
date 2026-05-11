281e9397101ea5fe0d015de14d8b50cb25c42777
unknown <ytw1214@naver.com>
2026-05-11 15:59:39 +0900

    feat : 크롤링 예외 추가

--- src/main/java/com/exerciting/Exerciting/Infrastructure/exception/ErrorCode.java
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/exception/ErrorCode.java
@@ -20,8 +20,9 @@ public enum ErrorCode {
     MATCHING_CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 채팅방을 찾을 수 없습니다."),
 
     INVALID_INPUT(HttpStatus.BAD_REQUEST,"잘못된 입력입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"서버 오류가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"서버 오류가 발생했습니다."),
 
+    CRAWLING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "크롤링 중 오류가 발생했습니다.");
 
     private final HttpStatus status;
     private final String message;
