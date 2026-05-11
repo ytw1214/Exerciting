bfbc3fdd146cf3ce566a3110884b05b906d8bda0
unknown <ytw1214@naver.com>
2026-05-11 18:14:58 +0900

    refactor : repository 어노테이션 추가 및 불필요한 메서드 삭제

--- src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChatRoom/repository/MatchingChatRoomRepository.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChatRoom/repository/MatchingChatRoomRepository.java
@@ -3,10 +3,10 @@ package com.exerciting.Exerciting.Domain.matching.Chat.matchingChatRoom.reposito
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatRoom.entity.MatchingChatRoom;
 import com.exerciting.Exerciting.Domain.matching.matching.entity.Matching;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface MatchingChatRoomRepository extends JpaRepository<MatchingChatRoom, Long> {
     Optional<MatchingChatRoom> findByMatching(Matching matching);
-    Optional<MatchingChatRoom> findByMatchingChatRoom(Long chatRoomId);
 }
