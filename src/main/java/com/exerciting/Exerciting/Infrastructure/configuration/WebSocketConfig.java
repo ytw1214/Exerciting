7331d1b950f339ad44aca653648da1f1c8f0b48c
unknown <ytw1214@naver.com>
2026-05-11 18:05:05 +0900

    refactor : 채팅 로직 수정

--- src/main/java/com/exerciting/Exerciting/Infrastructure/configuration/WebSocketConfig.java
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/configuration/WebSocketConfig.java
@@ -42,7 +42,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
                 if(accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
                     String token = accessor.getFirstNativeHeader("Authorization");
-                    if(token == null || jwtTokenProvider.validateToken(token.replace("Bearer ", ""))) {
+                    if(token == null || !jwtTokenProvider.validateToken(token.replace("Bearer ", ""))) {
                         throw new MessagingException("인증에 실패했습니다.");
                     }
                 }
