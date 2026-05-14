bd35462e168daedbc7131db70f86a2cfa556adfd
unknown <ytw1214@naver.com>
2026-05-14 18:13:49 +0900

    refactor : getmessages 메서드 로직 변경

--- src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/controller/MatchingChatController.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/controller/MatchingChatController.java
@@ -8,6 +8,7 @@ import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatRoom.entity.Ma
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatRoom.repository.MatchingChatRoomRepository;
 import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatRoom.service.MatchingChatRoomService;
 import com.exerciting.Exerciting.Domain.user.entity.User;
+import com.exerciting.Exerciting.Domain.user.entity.UserDetails;
 import com.exerciting.Exerciting.Domain.user.service.UserService;
 import com.exerciting.Exerciting.Exception.MatchingChatRoomNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 import java.util.List;
@@ -43,12 +48,13 @@ public class MatchingChatController {
                 chat.getSender().getName()
         );
     }
-
+    @GetMapping("/api/v1/chat/{chatRoomId}/messages")
+    @ResponseBody
     public ResponseEntity<List<ChatMessageResponseDto>> getMessages(
-            Long chatRoomId,
-            Principal principal) {
+            @PathVariable Long chatRoomId,
+            @AuthenticationPrincipal UserDetails userDetails) {
         MatchingChatRoom chatRoom = matchingChatRoomService.findByRoomId(chatRoomId);
-        User user = userService.getUserByUserId(principal.getName());
+        User user = userService.getUserByUserId(userDetails.getUsername());
         List<ChatMessageResponseDto> messages = matchingChatService.getMessages(chatRoom,user);
         if (messages.isEmpty()) {
             return ResponseEntity.noContent().build();
