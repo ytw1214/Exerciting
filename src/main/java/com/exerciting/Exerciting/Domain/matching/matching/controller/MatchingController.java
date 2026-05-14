9178081a473ae6df2d5332d3992f57a69900de73
unknown <ytw1214@naver.com>
2026-05-14 18:18:13 +0900

    refactor : 주입 변경

--- src/main/java/com/exerciting/Exerciting/Domain/matching/matching/controller/MatchingController.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/matching/controller/MatchingController.java
@@ -1,9 +1,11 @@
 package com.exerciting.Exerciting.Domain.matching.matching.controller;
 
+import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatRoom.service.MatchingChatRoomService;
 import com.exerciting.Exerciting.Domain.matching.matching.dto.MatchingRequestDto;
 import com.exerciting.Exerciting.Domain.matching.matching.entity.Matching;
 import com.exerciting.Exerciting.Domain.matching.matching.service.MatchingService;
 import com.exerciting.Exerciting.Domain.matching.matching.dto.MatchingResponseDto;
+import com.exerciting.Exerciting.Domain.matching.matchingParticipant.service.MatchingParticipantService;
 import com.exerciting.Exerciting.Domain.user.entity.User;
 import com.exerciting.Exerciting.Domain.user.entity.UserDetails;
 import com.exerciting.Exerciting.Domain.user.service.UserService;
@@ -24,15 +26,18 @@ import java.util.List;
 public class MatchingController {
     private final MatchingService matchingService;
     private final UserService userService;
-
+    private final MatchingChatRoomService matchingChatRoomService;
+    private final MatchingParticipantService matchingParticipantService;
     @PostMapping("/api/v1/Matching")
     public ResponseEntity<Long> saveMatching(
             @RequestBody MatchingRequestDto dto,
             @AuthenticationPrincipal UserDetails userDetails) {
         User user = userService.getUserByUserId(userDetails.getUserId());
-        Long data = matchingService.createMatching(dto, user.getId());
-
-        return ResponseEntity.ok(data);
+        Long matchingId = matchingService.createMatching(dto, user.getId());
+        Matching matching = matchingService.findById(matchingId);
+        matchingChatRoomService.createChatRoom(matching,user);
+        matchingParticipantService.joinMatching(matchingId, user.getId());
+        return ResponseEntity.ok(matchingId);
     }
 
 
