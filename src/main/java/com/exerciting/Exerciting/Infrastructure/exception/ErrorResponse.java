effacf7c213bed913fce1aa963f74bffdb6c7219
unknown <ytw1214@naver.com>
2026-05-15 17:22:38 +0900

    feat : ErrorResponse 응답 클래스 구현

---
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/exception/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.exerciting.Exerciting.Infrastructure.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+    private final String code;
+    private final String message;
+
+    private ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.name(), errorCode.getMessage());
+    }
+    public static ErrorResponse of(ErrorCode errorCode, String message) {
+        return new ErrorResponse(errorCode.name(), message);
+    }
+}
