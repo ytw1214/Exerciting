7248a6e28829275124f34c34dc8a834626332db8
unknown <ytw1214@naver.com>
2026-05-15 16:47:37 +0900

    refactor : @Param 어노테이션 적용

--- src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/repository/MatchingChatRepository.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/Chat/matchingChat/repository/MatchingChatRepository.java
@@ -5,12 +5,13 @@ import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatRoom.entity.Ma
 import com.exerciting.Exerciting.Domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MatchingChatRepository extends JpaRepository<MatchingChat, Long> {
-    @Query("select c from MatchingChat c join fetch c.sender where c.matchingChatRoom=:chatroom order by c.sendAt asc")
-    List<MatchingChat> findByMatchingChatRoomOrderBySendAtAsc(MatchingChatRoom matchingChatRoom);
+    @Query("select c from MatchingChat c join fetch c.sender where c.matchingChatRoom=:matchingChatRoom order by c.sendAt asc")
+    List<MatchingChat> findByMatchingChatRoomOrderBySendAtAsc(@Param("matchingChatRoom")MatchingChatRoom matchingChatRoom);
     long countByMatchingChatRoomAndSendAtAfter(MatchingChatRoom matchingChatRoom, LocalDateTime time);
 }
