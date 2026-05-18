78984d16c932e2db029b4d759454675383b1bf1c
unknown <ytw1214@naver.com>
2026-05-18 17:00:09 +0900

    refactor : MatchingStatus 필드 변경

--- src/main/java/com/exerciting/Exerciting/Domain/matching/matching/entity/MatchingStatus.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/matching/entity/MatchingStatus.java
@@ -1,8 +1,9 @@
 package com.exerciting.Exerciting.Domain.matching.matching.entity;
 
 public enum MatchingStatus {
-    RECRUTING("모집중"),
-    COMPLETED("모집 완료");
+    RECRUITING("모집중"),
+    FULL("인원 꽉 참"),
+    CLOSED("모집 완료");
 
     private String status;
 
