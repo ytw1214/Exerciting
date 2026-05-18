4a26066988385ea3cf9f40635451025dde0faaa0
unknown <ytw1214@naver.com>
2026-05-18 17:49:22 +0900

    refactor : 불필요한 import 문 제거

--- src/main/java/com/exerciting/Exerciting/Domain/matching/matchingParticipant/controller/MatchingParticipantController.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/matchingParticipant/controller/MatchingParticipantController.java
@@ -1,29 +1,30 @@
 package com.exerciting.Exerciting.Domain.matching.matchingParticipant.controller;
 
+import com.exerciting.Exerciting.Domain.matching.matchingParticipant.dto.MatchingParticipantDto;
 import com.exerciting.Exerciting.Domain.matching.matchingParticipant.service.MatchingParticipantService;
-import com.exerciting.Exerciting.Domain.user.entity.User;
-import com.exerciting.Exerciting.Domain.user.entity.UserDetails;
 import com.exerciting.Exerciting.Domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/participant")
 public class MatchingParticipantController {
     private final MatchingParticipantService matchingParticipantService;
     private final UserService userService;
-    @PostMapping("/{matchingId}/join")
-    public ResponseEntity<Void> joinMatching(
-            @PathVariable Long matchingId,
-            @AuthenticationPrincipal UserDetails userDetails) {
-        User user = userService.getUserByUserId(userDetails.getUsername());
-        matchingParticipantService.joinMatching(matchingId, user.getId());
-        return ResponseEntity.ok().build();
+    @GetMapping("/{matchingId}")
+    public ResponseEntity<List<MatchingParticipantDto>> getParticipants(
+            @PathVariable Long matchingId) {
+        List<MatchingParticipantDto> result = matchingParticipantService.getParticipants(matchingId);
+        if (result.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(result);
     }
 }
