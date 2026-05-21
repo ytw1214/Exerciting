eb8f7a8c925d2e678a71a6b30678c00e5c9db0dc
ytw1214 <ytw1214@naver.com>
2026-05-21 12:17:39 +0900

    refactor : 동시성 제어를 위한 @Lock 어노테이션 추가

--- src/main/java/com/exerciting/Exerciting/Domain/matching/matching/repository/MatchingRepository.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/matching/repository/MatchingRepository.java
@@ -1,9 +1,11 @@
 package com.exerciting.Exerciting.Domain.matching.matching.repository;
 
-import com.exerciting.Exerciting.Domain.matching.matching.dto.MatchingQueryResponseDto;
 import com.exerciting.Exerciting.Domain.matching.matching.entity.Matching;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,7 +15,9 @@ import java.util.Optional;
 @Repository
 public interface MatchingRepository extends JpaRepository<Matching, Long>, MatchingRepositoryCustom {
     List<Matching> findByMeetTimeBefore(LocalDateTime meetTime);
-    Optional<Matching> findById(Long id);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT m FROM Matching m WHERE m.id = :id")
+    Optional<Matching> findByIdWithLock(@Param("id") Long id);
     //List<MatchingQueryResponseDto> search(MatchingCustomCond cond);
 
 }
