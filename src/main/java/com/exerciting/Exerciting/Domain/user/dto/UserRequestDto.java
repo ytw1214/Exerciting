2026703ecff53e950197696782be82c27c4410a3
unknown <ytw1214@naver.com>
2026-05-26 10:56:36 +0900

    refactor : 유저 패턴 추가

--- src/main/java/com/exerciting/Exerciting/Domain/user/dto/UserRequestDto.java
+++ src/main/java/com/exerciting/Exerciting/Domain/user/dto/UserRequestDto.java
@@ -1,17 +1,28 @@
 package com.exerciting.Exerciting.Domain.user.dto;
 
 import com.exerciting.Exerciting.Domain.user.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserRequestDto(
+        @NotBlank(message="아이디를 입력해주세요")
+        @Size(min=4,max=20,message="아이디는 4~20자여야 합니다")
         String userId,
+
         @Pattern(
-                regexp="^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+                regexp="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
                 message = "패스워드는 영문자, 숫자, 특수기호를 포함하여 8자 이상이어야합니다."
         )
         String pw,
+        @NotBlank(message = "닉네임을 입력해주세요")
+        @Size(min = 2, max = 10, message = "닉네임은 2~10자여야 합니다")
         String nickname,
+        @NotBlank(message = "이름을 입력해주세요")
         String name,
+        @Email(message = "이메일 형식이 올바르지 않습니다")
+        @NotBlank(message = "이메일을 입력해주세요")
         String email
 ) {
     public User toEntity(String encodedPw) {
