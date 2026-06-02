3d9f65a30076b50738abe4c8d947d16514c6224e
unknown <ytw1214@naver.com>
2026-06-02 11:17:27 +0900

    refactor : 불필요한 import문 제거 및 getAllMatching 로직 변경

--- src/main/java/com/exerciting/Exerciting/Domain/matching/matching/controller/MatchingController.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/matching/controller/MatchingController.java
@@ -1,17 +1,15 @@
 package com.exerciting.Exerciting.Domain.matching.matching.controller;
 
-import com.exerciting.Exerciting.Domain.matching.Chat.matchingChatRoom.service.MatchingChatRoomService;
 import com.exerciting.Exerciting.Domain.matching.matching.dto.MatchingQueryResponseDto;
 import com.exerciting.Exerciting.Domain.matching.matching.dto.MatchingRequestDto;
 import com.exerciting.Exerciting.Domain.matching.matching.entity.Matching;
 import com.exerciting.Exerciting.Domain.matching.matching.repository.MatchingCustomCond;
 import com.exerciting.Exerciting.Domain.matching.matching.service.MatchingService;
 import com.exerciting.Exerciting.Domain.matching.matching.dto.MatchingResponseDto;
-import com.exerciting.Exerciting.Domain.matching.matchingParticipant.service.MatchingParticipantService;
-import com.exerciting.Exerciting.Domain.user.entity.User;
 import com.exerciting.Exerciting.Domain.user.entity.UserDetails;
 import com.exerciting.Exerciting.Domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -34,16 +32,11 @@ public class MatchingController {
         return ResponseEntity.ok(matchingId);
     }
     @GetMapping
-    public ResponseEntity<List<MatchingResponseDto>> getMatching() {
-        List<Matching> list = matchingService.getAllMatching();
-        List<MatchingResponseDto> result = list.stream()
-                .map(MatchingResponseDto::fromEntity)
-                .toList();
-
-        if(result.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.ok(result);
+    public ResponseEntity<Page<MatchingQueryResponseDto>> getMatching(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return ResponseEntity.ok(matchingService.getAllMatching(page,size));
     }
     @PostMapping("/{matchingId}/join")
     public ResponseEntity<Void> joinMatching(
