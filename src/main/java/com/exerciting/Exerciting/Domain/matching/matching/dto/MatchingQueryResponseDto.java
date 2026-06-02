7cc49339cd230e6308dcf07c511e2bd811dd32d5
unknown <ytw1214@naver.com>
2026-06-02 11:18:23 +0900

    feat : from 로직 구현

--- src/main/java/com/exerciting/Exerciting/Domain/matching/matching/dto/MatchingQueryResponseDto.java
+++ src/main/java/com/exerciting/Exerciting/Domain/matching/matching/dto/MatchingQueryResponseDto.java
@@ -1,5 +1,8 @@
 package com.exerciting.Exerciting.Domain.matching.matching.dto;
 
+
+import com.exerciting.Exerciting.Domain.matching.matching.entity.Matching;
+
 import java.time.LocalDateTime;
 
 public record MatchingQueryResponseDto(
@@ -12,4 +15,18 @@ public record MatchingQueryResponseDto(
         String stadiumName,
         String sportType
 ) {
+    public static MatchingQueryResponseDto from(Matching matching) {
+        return new MatchingQueryResponseDto(
+                matching.getId(),
+                matching.getTitle(),
+                matching.getMaxPerson(),
+                matching.getMeetTime(),
+                matching.getGame().getHomeTeam().getShortName(),
+                matching.getGame().getAwayTeam().getShortName(),
+                matching.getGame().getStadium().getName(),
+                matching.getGame().getSportType().name()
+
+                );
+
+    }
 }
