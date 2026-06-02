e3934c11cccfdd58948879790e58b52f2fac5597
unknown <ytw1214@naver.com>
2026-06-02 16:13:22 +0900

    feat : RefreshToken 엔티티 구현

---
+++ src/main/java/com/exerciting/Exerciting/Domain/user/entity/RefreshToken.java
@@ -0,0 +1,36 @@
+package com.exerciting.Exerciting.Domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+    private String token;
+    private LocalDateTime expiresAt;
+
+    @Builder
+    public RefreshToken(User user, String token, LocalDateTime expiresAt) {
+        this.user = user;
+        this.token = token;
+        this.expiresAt = expiresAt;
+    }
+
+    public void updateToken(String token, LocalDateTime expiresAt) {
+        this.token = token;
+        this.expiresAt = expiresAt;
+    }
+}
