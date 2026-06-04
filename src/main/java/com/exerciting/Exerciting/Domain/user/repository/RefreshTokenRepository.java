4a518eb5c09ebabebf664e274d5dc1e14513b95d
unknown <ytw1214@naver.com>
2026-06-04 15:32:38 +0900

    feat : RefreshToeknRepository 구현

---
+++ src/main/java/com/exerciting/Exerciting/Domain/user/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.exerciting.Exerciting.Domain.user.repository;
+
+import com.exerciting.Exerciting.Domain.user.entity.RefreshToken;
+import com.exerciting.Exerciting.Domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUser(User user);
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByUser(User user);
+}
