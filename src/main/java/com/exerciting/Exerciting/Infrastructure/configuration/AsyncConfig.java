53dbd49d4ef863efdb7826e044287a23ba2350e0
ytw1214 <ytw1214@naver.com>
2026-07-13 15:27:40 +0900

    wip : 멀티스레드 적용 구현중

--- src/main/java/com/exerciting/Exerciting/Infrastructure/configuration/AsyncConfig.java
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/configuration/AsyncConfig.java
@@ -1,8 +1,21 @@
 package com.exerciting.Exerciting.Infrastructure.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+    @Bean(name="kboExecutor")
+    public ThreadPoolTaskExecutor setKboExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+
+
+        return executor;
+    }
+
+
 }
