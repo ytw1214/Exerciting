e4313c80c3462ccb1b333db4ffa2e50ed6c3a5ab
unknown <ytw1214@naver.com>
2026-07-16 17:56:46 +0900

    feat : 멀티스레드 구성환경 설정 작성

--- src/main/java/com/exerciting/Exerciting/Infrastructure/configuration/AsyncConfig.java
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/configuration/AsyncConfig.java
@@ -5,17 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 @Configuration
 @EnableAsync
 public class AsyncConfig {
     @Bean(name="kboExecutor")
     public ThreadPoolTaskExecutor setKboExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(5);
+        executor.setThreadNamePrefix("kbo-date-crawling");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //shutdown시 task가 처리될때까지 대기
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        //종료될때까지 대기가 아닌 해당 시간만큼 대기 후 shutdown
+        executor.setAwaitTerminationSeconds(30);
 
         return executor;
     }
-
-
 }
