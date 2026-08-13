bc622a2bc84455c14527c74a92d9092499bd1057
ytw1214 <ytw1214@naver.com>
2026-08-13 23:30:47 +0900

    refactor : 메서드 네임명 변경

--- src/main/java/com/exerciting/Exerciting/Domain/user/repository/RefreshTokenRepository.java
+++ src/main/java/com/exerciting/Exerciting/Domain/user/repository/RefreshTokenRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByUser(User user);
-    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
     void deleteByUser(User user);
 }
