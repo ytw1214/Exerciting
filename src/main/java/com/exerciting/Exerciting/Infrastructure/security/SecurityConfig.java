c347d9e5a59655b5d2cfff0643c8a652916b6977
ytw1214 <117420027+ytw1214@users.noreply.github.com>
2026-08-13 14:36:12 +0900

    Security/access token modify (#3)
    * chore : 사용하지 않는 ThreadExamples 클래스 제거
    
    * chore : 불필요한 import문 제거

--- src/main/java/com/exerciting/Exerciting/Infrastructure/security/SecurityConfig.java
+++ src/main/java/com/exerciting/Exerciting/Infrastructure/security/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.exerciting.Exerciting.Infrastructure.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
