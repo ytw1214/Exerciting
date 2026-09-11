85b73b4ae922e25ef469f7e953b17f95130b8059
unknown <ytw1214@naver.com>
2026-09-11 11:33:24 +0900

    chore : JPA Auditing 및 스케쥴링 활성화

--- src/main/java/com/exerciting/Exerciting/ExercitingApplication.java
+++ src/main/java/com/exerciting/Exerciting/ExercitingApplication.java
@@ -2,7 +2,11 @@ package com.exerciting.Exerciting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class ExercitingApplication {
 	public static void main(String[] args) {
